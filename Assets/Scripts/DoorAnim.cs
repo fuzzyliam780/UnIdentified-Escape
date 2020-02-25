47f0556662dcd53cb1ef104d67f11d7a33f9544b
Aidan Varkoly <47114022+AidanVarkoly@users.noreply.github.com>
2020-02-24 21:45:39 -0500

    More stuff

---
+++ Assets/Scripts/DoorAnim.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnim : MonoBehaviour
+{
+
+    public Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            animator.SetBool("Door Open", true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine("ExitTimer");
+        }
+    }
+
+
+    IEnumerator ExitTimer()
+    {
+        yield return new WaitForSeconds(2);
+        animator.SetBool("Door Open", false);
+    }
+}
+
